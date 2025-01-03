71b9a96b622ea402d9e18aa6e00e08f49339a0f1
vukp <pukvavlovic10@gmail.com>
2025-01-03 01:10:54 +0100

    created a function, used the dom to select the container, loop over it using forEach and create a new empty div. Added class book for easier styling. Edited the innerHTML and appended it to the container

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ function Book(title, author, pages) {
 }
 
 function addBookToLibrary(title, author, pages) {
-  const newBook = new Book(title, author, pages);
-  library.push(newBook);
+  // create a new Book object
+  const book = new Book(title, author, pages);
+  // add it to library
+  library.push(book);
+  // call the displayBooks funciton
+  displayBooks();
+}
+
+// create a function which loops through the library array and displays books
+
+function displayBooks() {
+  const LibraryContainer = document.querySelector("#library-container");
+  LibraryContainer.innerHTML = "";
+  library.forEach((book, index) => {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book"); // added class to the newly created div in order to style it mroe easily
+    bookElement.innerHTML = `
+    <h2>${book.title}</h2>
+    Author: ${book.author}
+    Pages: ${book.pages}`;
+
+    LibraryContainer.appendChild(bookElement);
+  });
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310);
 addBookToLibrary("1984", "George Orwell", 328);
-
-console.log(library);
+addBookToLibrary("Lord Of the Rings", "J.R.R Tolkien", 399);
